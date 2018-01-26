8d1fb2f1c096ac43f4fb2eba66928e72f90bb72e
RobertMagier <34715949+robertmagier@users.noreply.github.com>
2018-01-25 19:47:28 -0500

    Working on listing all Employees

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,73 @@
 /*************Change this when creating new contract ************/
 adrSalaryMan = '0x1411cb266fced1587b0aa29e9d5a9ef3db64a9c5';
 
+
+function getEmployees()
+{
+  salaryManInstance.countEmployees((err,res) => {
+    if(res)
+      console.log(res.toNumber());
+  });
+
+
+ }
+
+
+
+function web3callback(error,res)
+    {
+      console.log("error: " + error + " "+ "result: " + res);
+
+    }
 /*Add new employee*/
+
+function addEmployeeFunction() {
+
+  fNameEmp = $('#fNameEmp').val()
+  lNameEmp = $('#lNameEmp').val()
+  adrEmp = $('#adrEmp').val()
+
+  //$('#contractsList').append("<tr><td>"+employees[elen]+"</tr>");
+   //elen +=1;
+
+   salaryManInstance.setEmployee(adrEmp,fNameEmp,lNameEmp, web3callback);
+
+   $('#addEmpPreloaderBtn').fadeOut();
+   $('#addEmpPreloaderBtn').fadeIn();
+   $('#addEmpBtn').prop("disabled", true).addClass("ui-state-disabled")
+   //console.log(salarymanInstance);
+}
+
+     $(document).ready(function(){
+       getEmployees();
+       $("#salaryManAdress").hide().text("Salary Man: " + adrSalaryMan).fadeIn(500).effect("bounce",{ times: 3, distance:15 }, "slow");
+       $("#salaryManBalance").text("test " + getBalance(adrSalaryMan,"#salaryManBalance")).hide().fadeIn(500);
+
+        $('#addEmpBtn').click(addEmployeeFunction);
+        $("#updateContract").click(function(){
+          $("#test").hide();
+          $("#test").text("Contract: " + $("#contractNumber").val());
+        $("#test").fadeIn(500);
+        $("#test").effect( "bounce", { times: 3, distance:15 }, "slow" );
+        var balance = getBalance($("#contractNumber").val(),'#balance');
+        console.log(balance);
+        $("#balance").text("test " + getBalance($("#contractNumber").val()));
+
+
+
+
+
+ });
+
+     $("#sendfunds").click(function (){
+       var transaction = {
+         from: $('#contractNumber').val(),
+         to: $('#sendToContractNumber').val(),
+         value: web3.toWei($('#sendToAmount').val(),"ether")
+       };
+       web3.eth.sendTransaction(transaction,web3callback);
+     })
+});
 
 
 
@@ -30,12 +96,28 @@ if (typeof web3 !== 'undefined') {
   //filter.watch(function (error, log) {
   //console.log(log); //  {"address":"0x0000000000000000000000000000000000000000", "data":"0x0000000000000000000000000000000000000000000000000000000000000000", ...}
   //  });
-event = salaryManInstance.PaymentRecived("latest");
-event.watch(function(error, result) {
+  event = salaryManInstance.PaymentRecived("latest");
+  event.watch(function(error, result) {
   if (!error) {
       console.log("PaymentRecived");
       //$("#salaryManBalance").text(getBalance(adrSalaryMan,'#salaryManBalance')).hide().fadeIn(500);
       console.log(result.args.amount.toNumber());
+    }
+  });
+
+      eventemp = salaryManInstance.employeeInfo();
+      eventemp.watch(function(error, result) {
+        if (!error) {
+            console.log("EmployeeInfo Event");
+          //  $('#addEmpPreloaderBtn').fadeOut(500);
+            //$("#salaryManBalance").text(getBalance(adrSalaryMan,'#salaryManBalance')).hide().fadeIn(500);
+            $('#addEmpPreloaderBtn').fadeOut();
+            $('#addEmpBtn').prop("disabled", false).removeClass("ui-state-disabled")
+            //$('#fNameEmp').val('');
+            //$('#lNameEmp').val('');
+            //$('#adrEmp').val('');
+            console.log(result.args);
+            getEmployees();
 
 }});
 
